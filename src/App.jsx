5399d0afb1b244058923396d696d69c376cc1ca5
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-06-11 09:16:57 -0400

    Working on loop for countdown.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,34 @@ import GameCard from './GameCard.jsx';
 
 
 const App = () => {
-  return (
   
-      <>
-        
-        <h1>Whack A Mole</h1>
 
-        <section className = 'countdown'> {/* Working out the 15 second countdown */ }
+for (let i = 15; i < 15; i--) {
+  console.log("Count is", i);
+}
+
+
+
+
+  <section className = 'countdown'> /* Working out the 15 second countdown */ 
           {setInterval(() => {
             (console.log('15 seconds passed'));
           }, 15000)}
 
         </section>
+  
+  
+  
+  
+  
+  return (
+  
+
+      <>
+        
+        <h1>Whack A Mole</h1>
+
+        
           <main>
             <Welcome />
             <GameCard />
